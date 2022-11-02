dea860773f1e3af2f95369c6a4dfbd09f7c3c4f1
Your Name <you@example.com>
2022-11-02 11:02:15 +0530

    hello

---
+++ dom.js
@@ -0,0 +1,12 @@
+//window.alert(1);
+//document.all[10].textContent="hello";
+//console.log(document.domain);
+//console.log(document.URL);
+
+//document.title="hello";
+//console.log(document.title);
+document.all[10].textContent="hello";
+//var headertitle=document.getElementById("header-title");
+//headertitle.textContent="hello";
+var items=document.getElementsByClassName('list-group-item');
+items[1].textContent="goodbye";
