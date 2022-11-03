6f2682112bac323cd50e47b0d5869dc964a8f40f
Your Name <you@example.com>
2022-11-03 15:32:23 +0530

    sathish

--- practice/p.js
+++ practice/p.js
@@ -1,5 +1,5 @@
 var li=document.getElementsByTagName("li");
 li[1].textContent="hello";
 for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor="orange";
+    li[i].style.backgroundColor="red";
 }
